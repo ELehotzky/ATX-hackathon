96a84aeca99f0edb95efb8e641901288c6176123
Melanie <melanienvillela@gmail.com>
2020-09-19 21:22:06 -0500

    Layout Update
    Updated layout, added input, added state.

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import './App.css';
+import * as d3 from 'd3';
 
-function App() {
-  return (
-    <div className="main">
-      
-    </div>
-  );
+class App extends React.Component {
+	state = {
+		race: []
+	}
+
+	handleChange = (e) => {
+		console.log(e.target.id)
+		if(e.target.id.checked == true) {
+			console.log("YES")
+		}
+
+		this.setState({
+			race: e.target.value
+		});
+	}
+
+	render() {
+		return (
+			<div className="main">
+				<div className="container">
+					<h1>HOW BIG IS YOUR MOUNTAIN?</h1>
+					<div className="selection">
+						<form onChange={this.handleChange}>
+							<input type="checkbox" id="white" name="white" value="white"></input>
+							<label htmlFor="white">WHITE</label>
+							<input type="checkbox" id="black" name="black" value="black"></input>
+							<label htmlFor="black">BLACK</label>
+							<input type="checkbox" id="hispanic" name="hispanic" value="hispanic"></input>
+							<label htmlFor="hispanic">HISPANIC</label>
+						</form>
+					</div>
+				</div>
+				<div className="container" ref="canvas"></div>
+			</div>
+		);
+	}
 }
 
 export default App;
+
+//
