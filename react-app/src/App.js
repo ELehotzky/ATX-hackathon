00b59b923cd3dfbcef58dccf73e6990319bffe99
Melanie <melanienvillela@gmail.com>
2020-09-20 02:22:02 -0500

    Update
    Added Checkbox and Linechart components.

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,40 +1,39 @@
 import React from 'react';
 import './App.css';
+import Checkbox from './Components/Checkbox';
+import Linechart from './Components/Linechart';
 import * as d3 from 'd3';
 
 class App extends React.Component {
-	state = {
-		race: []
-	}
-
-	handleChange = (e) => {
-		console.log(e.target.id)
-		if(e.target.id.checked == true) {
-			console.log("YES")
+	//In the state below we can import the data that we collected and depending on if the checkbox state is "checked" is true we can display the data
+	state = [
+		{
+			race: "WHITE",
+		},		
+		{
+			race: "BLACK",
+		},
+		{
+			race: "HISPANIC",
 		}
-
-		this.setState({
-			race: e.target.value
-		});
-	}
+	]	
 
 	render() {
+		const people = this.state;
+	
 		return (
 			<div className="main">
 				<div className="container">
 					<h1>HOW BIG IS YOUR MOUNTAIN?</h1>
 					<div className="selection">
-						<form onChange={this.handleChange}>
-							<input type="checkbox" id="white" name="white" value="white"></input>
-							<label htmlFor="white">WHITE</label>
-							<input type="checkbox" id="black" name="black" value="black"></input>
-							<label htmlFor="black">BLACK</label>
-							<input type="checkbox" id="hispanic" name="hispanic" value="hispanic"></input>
-							<label htmlFor="hispanic">HISPANIC</label>
-						</form>
+						{people.map((item, index) =>
+							<Checkbox name={item.race} key={index}></Checkbox>
+						)}							
 					</div>
 				</div>
-				<div className="container" ref="canvas"></div>
+				<div className="container" id="mountain">
+					<Linechart></Linechart>
+				</div>
 			</div>
 		);
 	}
